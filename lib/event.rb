a426b57de43e4b5583845c2c5e681a8142ea37ed
Antoine Stjepanovic <apbstep@gmail.com>
2020-10-12 19:33:23 +0200

    final commit

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,65 @@
 class Event
+	
+	attr_accessor :start_date
+	attr_accessor :duration
+	attr_accessor :title
+	attr_accessor :attendees
+
+
+	def initialize(event_date, event_duration, event_title, event_attendees)
+		@start_date = Time.parse(event_date)
+		@duration = event_duration
+		@title = event_title
+		@attendees = event_attendees
+
+	end 
+
+	def postpone_24h
+		@start_date = @start_date + 86400
+	end
+
+	def end_date
+		return @start_date + @duration * 60
+	end
+
+	def is_past?
+		if @start_date < Time.now
+			return true
+		else
+			return false
+		end
+	end
+
+	def is_futur?
+		if @start_date > Time.now
+			return true
+		else
+			return false
+		end
+	end
+
+	def is_soon?
+		if @start_date - Time.now < 1800
+			return true
+		else
+			return false
+		end
+	end
+
+	def to_s
+		strfy = ""
+		@attendees.each{|attendee| 
+			if attendee != @attendees.last 
+				strfy = strfy + attendee + ", "
+			else 
+				strfy = strfy + attendee 
+			end
+			}
+
+		puts ">Titre : #{@title}"
+		puts ">Date de début : #{@start_date}"
+		puts ">Durée : #{@duration} minutes"
+		puts ">Invité.e.s : #{strfy}"
+	end
 
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,31 @@
 class User
 
+	attr_accessor :email 
+	attr_accessor :age
+	@@all_user =[]
+
+	
+
+	def initialize(user_email, user_age)
+		@email = user_email
+		@age = user_age
+		@@all_user.push(self)
+
+	end
+
+	def self.all
+		puts @@all_user
+	end
+
+	def self.find_by_email(email)
+		@@all_user.each{|user|
+			if user.email = email 
+				return user
+
+			end
+		}
+
+	end
+
 end
+
